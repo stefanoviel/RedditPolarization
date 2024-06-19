a9b1ba89dbf0fde7be80c7548bb919b5dca69d67
Stefano Viel <stefanoviel@coss-l01.intern.ethz.ch>
2024-06-19 17:22:37 +0200

    parallelized writing to duck db

--- config.py
+++ config.py
@@ -4,39 +4,12 @@ import time
 # add timestamp to not have same experiments with overlapping names
 timestamp = time.strftime("%Y%m%d_%H%M%S") 
 
-EXPERIMENT_NAME = "first"
-OUTPUT_PATH = os.path.join("/cluster/work/coss/stviel", f"{timestamp}_{EXPERIMENT_NAME}")
 
+OUTPUT_DIR = "/media/data/stviel/RedditPolarization/output"  # where the experiment will be saved
 
+EXPERIMENT_NAME = "testing_duckdb"
+OUTPUT_PATH = os.path.join(OUTPUT_DIR, f"{timestamp}_{EXPERIMENT_NAME}")
 
-
-
-
-
-
-# OLD parameters
-
-MONTHS_COMMENTS = []
-MONTHS_SUBMISSIONS = ['05', '06']
-
-EMBEDD_SET = ['S']
-ANALYSE_SET = 'S'
-
-MODEL_PATH = f'/cluster/work/coss/anmusso/victoria/model/all-mpnet-base-v2'
-INPUT_FILE_BASE_PATH = '/cluster/work/coss/anmusso/reddit'
-EMBEDINGS_BASE_PATH = '/cluster/work/coss/anmusso/victoria/embeddings' #where embeddingfiles will be saved
-DATA_BASE_PATH = '/cluster/work/coss/anmusso/victoria/loaded_data/loaded_data.db'
-
-MODEL_NAME = 'all-mpnet-base-v2' # available models: 'all-mpnet-base-v2' , 'all-MiniLM-L6-v2', 'multi-qa-mpnet-base-dot-v1'
-
-FILTER_UMAP= False
-UMAP_COMPONENTS = 5
-UMAP_N_Neighbors = 50
-UMAP_MINDIST = 0.01
-
-HDBS_MIN_CLUSTERSIZE= 50
-#HDBS_ALG = ''
-
-TERMS = ['blm', 'Black', 'Lives', 'Matter', 'BLM', 'racist', 'racism', 'defund', 'police', 'George', 'Floyd']
-SUBREDDIT = 'BlackLivesMatter'
+REDDIT_DATA_DIR = 'data'
+REDDIT_DB_FILE = 'data/dbs/duck_db_test.db'
 
--- src/load_data_to_db.py
+++ src/load_data_to_db.py
@@ -1,87 +1,161 @@
-
 import os
 import sys 
 # adding root directory to paths
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from logging_config import configure_get_logger
 import config
-logger = configure_get_logger(__name__)
+if not os.path.exists(config.OUTPUT_PATH):
+    os.makedirs(config.OUTPUT_PATH)
+logger = configure_get_logger(config.OUTPUT_PATH)
+os.environ['NUMEXPR_MAX_THREADS'] = '32'  
+import numexpr
 
 
 from src.unpack_zst import read_lines_zst
 from typing import Dict, Any, Optional
 import json
+import duckdb
+from tqdm import tqdm
+import zstandard as zstd
+from concurrent.futures import ProcessPoolExecutor, as_completed
+import threading
+import time
+import duckdb
 
-    
-def keep_relevant_submissions(line_json: Dict[str, Any], min_num_characters: int = 20) -> Optional[Dict[str, Any]]:
-    """Filters out submission with a lenght lower than `min_num_characters"""
-    if len(line_json.get('title', '') + line_json.get('selftext', '') ) > min_num_characters:
+# Global counter and lock for thread-safe operations
+row_count = 0
+row_count_lock = threading.Lock()
+stop_monitor = False
+ 
+def keep_relevant_submissions(line_json, min_num_characters=20):
+    if len(line_json.get('title', '') + line_json.get('selftext', '')) > min_num_characters:
         desired_attributes = {'subreddit', 'score', 'author', 'created_utc', 'title', 'id', 'num_comments', 'upvote_ratio', 'selftext'}
         return {key: line_json[key] for key in desired_attributes if key in line_json}
     return None
 
 
-def create_submissions_table(table_name, sql_db):
-    """Creates a table in the database if it doesn't exist."""
-    sql_db.sql(f"DROP TABLE IF EXISTS {table_name}")
-    sql_db.sql(
-        f"CREATE TABLE {table_name} (NUM INT PRIMARY KEY , score INT, subreddit VARCHAR, author VARCHAR, created_utc INTEGER, "
-        "title VARCHAR , id VARCHAR, parent_id VARCHAR, body VARCHAR)")
-    
+def setup_database(db_file):
+    # Establish a connection to handle schema setup.
+    conn = duckdb.connect(db_file)
+    conn.execute("DROP TABLE IF EXISTS submissions")
+    conn.execute("""CREATE TABLE IF NOT EXISTS submissions (
+        subreddit TEXT,
+        score INTEGER,
+        author TEXT,
+        created_utc INTEGER,
+        title TEXT,
+        id TEXT PRIMARY KEY,
+        num_comments INTEGER,
+        upvote_ratio REAL,
+        selftext TEXT
+    )""")
+    conn.close()
 
-def _add_to_db_json(good_lines, table_name, sql_db):
-    with open(temp_file_path_j, 'w') as f:
-        json.dump(good_lines, f)
-    sql_db.execute(
-        f"COPY {table_name} FROM '{temp_file_path_j}' (FORMAT JSON, AUTO_DETECT true) ;")
-    #print("added to collection", len(good_lines))
-    
 
-def extract_submissions(input_file_name: str, sql_db, table_name: str):
-    reader_chunk_size: int = 2 ** 27
-    reader_window_size: int = 2 ** 31
-    good_lines = []
+def process_file(file_path, db_file, index, batch_size=100):
+    print(f"Thread {index} started")
+    local_conn = duckdb.connect(db_file).cursor()  # Create a DuckDB cursor for the thread
 
-    if ((table_name,) in sql_db.execute("SHOW TABLES;").fetchall()):
-        max_id = sql_db.sql(f"SELECT MAX(num) FROM {table_name}").fetchone()
-        if max_id[0] is not None:
-            good_lines_count = max_id[0] + 1
-        else: 
-            good_lines_count = 0
-    else: 
-        create_submissions_table(table_name, sql_db)
-        good_lines_count = 0
-    #initialize tracking variables
-    bad_lines, file_lines, file_bytes_processed = 0, 0, 0
-    file_size = os.stat(input_file_name).st_size
+    buffer = []  # Buffer to hold data rows before batch insert
 
-    # Loop through every line in the file
-    for line, file_bytes_processed in read_lines_zst(file_name=input_file_name, reader_window_size=reader_window_size,
-                                                     reader_chunk_size=reader_chunk_size):
-        try:
-            line_json = json.loads(line)
-            filtered_line = _filter_submission(line_json)  #do basic preselection of lines
-            if filtered_line is not None:
-                filtered_line['NUM'] = good_lines_count
-                good_lines_count += 1
-                good_lines.append(filtered_line)  #add to list of valid lines
+    with open(file_path, 'rb') as file_handle:
+        reader = zstd.ZstdDecompressor(max_window_size=2147483648).stream_reader(file_handle)
+        while True:
+            chunk = reader.read(2**27)  # Read 128 MB at a time
+            if not chunk:
+                break
+            data = chunk.decode('utf-8').split('\n')
+            for line in data[:-1]:
+                try:
+                    line_json = json.loads(line)
+                    filtered_data = keep_relevant_submissions(line_json)
+                    if filtered_data:
+                        buffer.append(filtered_data)
+                        if len(buffer) >= batch_size:
+                            insert_into_db(local_conn, buffer)
+                            buffer.clear()
+                            with row_count_lock:
+                                global row_count
+                                row_count += batch_size 
+                except json.JSONDecodeError:
+                    continue
 
-        except (KeyError, json.JSONDecodeError) as err:
-            bad_lines += 1
-        file_lines += 1
+    # Insert any remaining rows in the buffer after loop ends
+    if buffer:
+        insert_into_db(local_conn, buffer)
 
-        # Log progress
-        if file_lines % 200_000 == 0:
-            logger.info(
-                f": {good_lines_count:,} {file_lines:,} : {bad_lines:,} : {file_bytes_processed:,}:{(file_bytes_processed / file_size) * 100:.0f}%")
+    print(f"Thread {index} finished")
 
-        # Write the lines to the db file
-        if len(good_lines) > 10_000:  #for now only opening json file once is fastest..
-            _add_to_db_json(good_lines, table_name, sql_db)
-            good_lines = []
+def insert_into_db(connection, data_batch):
+    if not data_batch:
+        return
 
-    #add last bit of lines
-    _add_to_db_json(good_lines, table_name, sql_db)
+    # Assuming all data in batch have the same keys
+    expected_keys = {'subreddit', 'score', 'author', 'created_utc', 'title', 'id', 'num_comments', 'selftext'}
+    columns = ', '.join(data_batch[0].keys())
+    placeholders = ', '.join(['?'] * len(data_batch[0]))
+    query = f"INSERT INTO submissions ({columns}) VALUES ({placeholders})"
 
-    print('total added lines: ', sql_db.sql(f"SELECT COUNT(*) FROM {table_name}"))
-    logger.info(f"Complete : {file_lines:,} : {bad_lines:,} : {good_lines_count:,}")
+   # Filter and validate data entries
+    valid_data_batch = []
+    for row in data_batch:
+        if set(row.keys()) == expected_keys and None not in row.values():
+            valid_data_batch.append(row)
+
+    # Prepare values for insertion
+    values = [tuple(row[key] for key in expected_keys) for row in valid_data_batch]
+
+    try:
+        if values:
+            connection.begin()
+            connection.executemany(query, values)
+            connection.commit()
+        else:
+            print("No valid data to insert.")
+    except Exception as e:
+        print(f"Error during database insert operation: {e}")
+        connection.rollback()
+
+    # if len(valid_data_batch) < len(data_batch):
+    #     print(f"Removed {len(data_batch) - len(valid_data_batch)} invalid entries out of {len(data_batch)} total.")
+
+
+def monitor_rows():
+    global stop_monitor
+    while not stop_monitor:
+        with row_count_lock:
+            print(f"Total rows written: {row_count:,}", end='\r')
+        time.sleep(1)  # Update every second
+
+def main(directory, db_file):
+    global stop_monitor
+
+    setup_database(db_file)
+
+    threads = []
+    count = 0 
+    for file_name in os.listdir(directory):
+        if file_name.endswith('.zst'):
+            file_path = os.path.join(directory, file_name)
+            thread = threading.Thread(target=process_file, args=(file_path, db_file, count, 20000))
+            threads.append(thread)
+            thread.start()
+            count += 1
+
+    monitor_thread = threading.Thread(target=monitor_rows)
+    monitor_thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    stop_monitor = True
+    monitor_thread.join()
+    print(f"Final total rows written: {row_count}")
+
+
+
+if __name__ == '__main__':
+
+    logger.info("Loading data to DuckDB")
+    main(config.REDDIT_DATA_DIR, config.REDDIT_DB_FILE)
+    logger.info("Data loaded successfully")

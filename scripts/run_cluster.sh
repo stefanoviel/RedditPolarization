34392517b49d1a7c49955607974702f3c2f03ede
Stefano Viel <stefanoviel@coss-l01.intern.ethz.ch>
2024-07-28 10:54:54 +0200

    run cluster

---
+++ scripts/run_cluster.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=filtered_pipeline
+#SBATCH --nodes=1
+#SBATCH --gpus=rtx_4090:1
+#SBATCH --mem-per-cpu=60GB
+#SBATCH --time=48:00:00
+#SBATCH --output=log_scripts/%x-%j.out
+
+module load stack/.2024-05-silent  gcc/13.2.0 python_cuda/3.11.6
+echo "loaded stack"
+
+source .venv/bin/activate
+export OMP_NUM_THREADS=16
+ulimit -n 4096  # to allow duckdb to open all files
+
+
+python scripts/main.py
